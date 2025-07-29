1bb2db5526bc8b1a94b53d44b16c180a6ebad1e8
Programador Fred <fredericoraposinha@gmail.com>
2025-07-29 01:15:40 -0300

    Corrigido Main.lua para usar loadstring.lua diretamente

--- Main.lua
+++ Main.lua
@@ -2,29 +2,16 @@
 -- Criado por K9zzzzz
 -- Carregamento principal do script
 
-local function LoadScript()
-    -- Carregar módulos
-    local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Progamador-Fred/K9-FrameWork/main/modules/UI.lua'))()
-    local Numbers = loadstring(game:HttpGet('https://raw.githubusercontent.com/Progamador-Fred/K9-FrameWork/main/modules/Numbers.lua'))()
-    local Notification = loadstring(game:HttpGet('https://raw.githubusercontent.com/Progamador-Fred/K9-FrameWork/main/modules/Notification.lua'))()
-    
-    -- Inicializar o script
-    local AutoJJs = loadstring(game:HttpGet('https://raw.githubusercontent.com/Progamador-Fred/K9-FrameWork/main/modules/AutoJJs.lua'))()
-    
-    -- Configurações padrão
-    local Options = {
-        Keybind = Enum.KeyCode.RightControl,
-        Tempo = 2.5,
-        Rainbow = false,
-        Language = {
-            UI = 'pt-br',
-            Words = 'pt-br'
-        }
+-- Configurações padrão
+local Options = {
+    Keybind = Enum.KeyCode.RightControl,
+    Tempo = 2.5,
+    Rainbow = false,
+    Language = {
+        UI = 'pt-br',
+        Words = 'pt-br'
     }
-    
-    -- Iniciar o script
-    AutoJJs:Initialize(Options)
-end
+}
 
--- Executar o script
-LoadScript() 
+-- Carregar e executar o script
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Progamador-Fred/K9-FrameWork/main/loadstring.lua'))()(Options) 
